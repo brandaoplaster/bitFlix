d3eecd6c6a053f84c8e5746f63c12aec34cad6f0
brandaoplaster <brandaoplaster@gmail.com>
2020-05-20 21:42:29 -0300

    Controller search action index that returns the users search

--- app/controllers/api/v1/searches_controller.rb
+++ app/controllers/api/v1/searches_controller.rb
@@ -1,6 +1,9 @@
 class Api::V1::SearchesController < ApplicationController
  
   def index
+    check_search_value and (return if performed?)
+    search = PgSearch.multisearch(params[:value]).order("created_at DESC")
+    reder json: Api::V1::WatchableSerializer.new(search.map(&:searchble)).serialized_json
   end
 
   private
