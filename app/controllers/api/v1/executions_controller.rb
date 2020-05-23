933e4fa97845a680307369b48e6c67bf2d50777f
brandaoplaster <brandaoplaster@gmail.com>
2020-05-23 19:29:06 -0300

    Action show presents the movie or series that the user is watching

--- app/controllers/api/v1/executions_controller.rb
+++ app/controllers/api/v1/executions_controller.rb
@@ -1,7 +1,10 @@
 class Api::V1::ExecutionsController < ApplicationController
   before_action :set_execution, only: :update
   
-  def create
+  def show
+    movie = Movie.find(params[:id])
+    @player = movie.players.find_or_create_by(end_date: nil, user: current_user)
+    render json: Api::V1::PlayerSerializer.new(@player, include: [:movie]).serialized_json
   end
 
   def update
